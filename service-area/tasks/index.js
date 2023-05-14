c1a1f96c98e57b38fbc8699ef3d42737da4ae46e
Michael Holod <git_ara@ukr.net>
2023-05-14 23:57:19 +0300

    This file is used as an entry point in the folder. It provides re-export of enteties from the related files

---
+++ service-area/tasks/index.js
@@ -0,0 +1,10 @@
+//Tasks re-export
+export { clean } from "./clean.js";
+export { cleanSync } from "./cleanSync.js";
+export { styles } from "./styles.js";
+export { scripts } from "./scripts.js";
+export { transpilation } from "./ts-transpilation.js";
+export { images } from "./images.js";
+export { templates } from "./templates.js";
+export { ttfToWoff, ttfToWoff2 } from "./fonts.js";
+export { server } from "./server.js";
