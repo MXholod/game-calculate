8a8e7baacbff181499efe4436fd0373c3140cc94
Michael Holod <git_ara@ukr.net>
2023-12-20 00:55:48 +0200

    Types for the current-amount block have been created

---
+++ src/scripts/types/game-current-amount.ts
@@ -0,0 +1,5 @@
+//Input field displays an amount of user's points. Boolean highlights border color when correct/incorrect 
+export type LevelUserAmount = [number, boolean]; 
+
+//Input field displays an amount of CPU's points. Boolean hides/shows an amount at the end of the level
+export type LevelCpuAmount = [number, boolean];
