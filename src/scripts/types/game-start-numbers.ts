06cb295e7a96103c086ea11f815cff626e43d385
Michael Holod <git_ara@ukr.net>
2024-01-22 02:06:39 +0200

    Added types for 'game-start-numbers'

--- src/scripts/types/game-start-numbers.ts
+++ src/scripts/types/game-start-numbers.ts
@@ -1,9 +1,46 @@
 export type ButtonNumberMode = [string, boolean];
-export type ButtonRandom = [number, boolean];
+export type RangeNumbers = [string, string];
 
-interface IStartNumbersBlock<BtnMode, BtnRand>{
-	buttonMode: Array<BtnMode>,
-	buttonRandom: BtnRand
+interface ISliderBlockNodes{
+	mainSliderBlock: HTMLDivElement | null;
+	outputFrom: HTMLOutputElement | null;
+	outputTo: HTMLOutputElement | null;
+	leftSlider: HTMLInputElement | null;
+	rightSlider: HTMLInputElement | null;
 }
 
-export type StartNumsBlock = IStartNumbersBlock<ButtonNumberMode, ButtonRandom>;
+interface IRandomNums{
+	leftInp: HTMLInputElement | null;
+	rightInp: HTMLInputElement | null;
+}
+
+interface IStartNumbersBlock<BtnMode, RangeNums>{
+	buttonMode: Array<BtnMode>;
+	rangeNumbers: RangeNums;
+	sliderBlockNodes: ISliderBlockNodes;
+	randomInputs: IRandomNums;
+}
+
+export type StartNumsBlock = IStartNumbersBlock<ButtonNumberMode, RangeNumbers>;
+
+export type chooseRadioMode = (event: Event)=>void;
+
+export type rememberRangeSliderNodes = (timer:HTMLDivElement)=>boolean;
+
+export type updateRangeSliderNodes = (currentLevelIndex: number)=>boolean;
+
+export type processRangeMode = (rangeMode: string)=>boolean;
+
+export type changeModeToCurrent = (numValue:string)=>void;
+
+export type displayRange = (rangeInput:HTMLInputElement, recalc?:boolean)=>string;
+
+export type saveInpValWithRecalc = (elemName:string, value:string, recalc:boolean)=>(string | undefined);
+
+export type recalculateValToMode = ()=>void;
+
+export type rangeNumsKeyboard = (rangeInput:HTMLInputElement)=>string;
+
+export type turnOnOffStartNums = (onOrOff:boolean)=>boolean;
+
+export type resetValsOnEndLevel = ()=>void;
