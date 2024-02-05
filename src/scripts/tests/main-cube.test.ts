c4ecb4bfe39d0e18f3d885655cbaf02ab2dee925
Michael Holod <git_ara@ukr.net>
2024-02-06 01:17:42 +0200

    Added tests for the 'main cube' functions

---
+++ src/scripts/tests/main-cube.test.ts
@@ -0,0 +1,62 @@
+/**
+* @jest-environment jsdom
+*/
+import { ISQuareBlock } from './../types/main-cube';
+import { startCubeAnimation, backCubeAnimation, appllyNumsToCube, clearNumsOfCube } from './../main-cube';
+//'testStartCubeAnimation'
+const parentTimer:HTMLElement = document.createElement('ASIDE') as HTMLElement; 
+const timer:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+	parentTimer.appendChild(timer);
+const content:HTMLDivElement = document.createElement('SECTION') as HTMLDivElement;
+const contentGame:HTMLDivElement = document.createElement('SECTION') as HTMLDivElement;
+const levelBoard:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+const contentMain:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+const threeDWrapper:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+const threeD:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+	threeDWrapper.appendChild(threeD);
+	contentMain.appendChild(threeDWrapper);
+	contentGame.appendChild(levelBoard);
+	contentGame.appendChild(contentMain);
+	content.appendChild(contentGame);
+	content.appendChild(parentTimer);
+//'testAppllyNumsToCube'
+const cells: HTMLDivElement[] = [ 
+	document.createElement("DIV") as HTMLDivElement,
+	document.createElement("DIV") as HTMLDivElement
+];
+const nums:string[] = ["(+) 34", "(*) 221"];
+const mainCube:HTMLDivElement = document.createElement('DIV') as HTMLDivElement; 
+	threeD.appendChild(mainCube);
+
+const blocks:ISQuareBlock = {
+	mainS: contentMain,
+	threeBW: threeDWrapper,
+	threeB: threeD
+}
+	
+describe("Tests for the 'Main Cube' block",()=>{
+	test("Starting the cube animation. The scale of the cube increases",()=>{
+		const testStartCubeAnimation = jest.fn(startCubeAnimation);
+		testStartCubeAnimation(timer);
+		expect(testStartCubeAnimation).toHaveBeenCalled();
+		expect(testStartCubeAnimation(timer)).toBeTruthy();
+	});
+	test("Rewind the cube animation. The scale of the cube decreases",()=>{
+		const testBackCubeAnimation = jest.fn(backCubeAnimation);
+		testBackCubeAnimation();
+		expect(testBackCubeAnimation).toHaveBeenCalled();
+		expect(testBackCubeAnimation(()=>{})).toBeTruthy();
+	});
+	it("Merging elements with values", ()=>{
+		const testAppllyNumsToCube = jest.fn(appllyNumsToCube);
+		testAppllyNumsToCube(cells, nums);
+		expect(testAppllyNumsToCube).toHaveBeenCalled();
+		expect(testAppllyNumsToCube(cells, nums)).toBeTruthy();
+	});
+	it("Clear child elements", ()=>{
+		const testClearNumsOfCube = jest.fn(clearNumsOfCube);
+		testClearNumsOfCube();
+		expect(testClearNumsOfCube).toHaveBeenCalled();
+		expect(testClearNumsOfCube()).toBeTruthy();
+	});
+});
