d0c19998cc338e61ef5fe8252e18a9322efd9bce
Michael Holod <git_ara@ukr.net>
2024-07-07 19:30:35 +0300

    Added button state types and table statistics types. Three types of functions, one to handle a button click, one for changing the state of the button and one for caching table nodes

--- src/scripts/types/game-statistics.ts
+++ src/scripts/types/game-statistics.ts
@@ -1,11 +1,34 @@
-export interface ILevelStatistics{
-	idLevel: number;
-	pointsLevel: number;
-} 
+type btnLabel = "Open game statistics" | "Close game statistics";
 
-export type LevelList = Array<ILevelStatistics>;
+export interface IButton{
+	btn: (null | HTMLButtonElement);
+	stateBtn: boolean;
+	labelBtn: btnLabel;
+}
 
-export interface IStatistics extends ILevelStatistics{
-	totalLevelAmount: number;
-	buttonState: boolean;
-} 
+export interface IRow{
+	orderNum: (HTMLDivElement | null);
+	time: (HTMLDivElement | null);
+	userRes: (HTMLDivElement | null);
+	cpuRes: (HTMLDivElement | null);
+	isSuccess: (HTMLDivElement | null);
+}
+
+export interface IRows extends IRow{
+	visible: boolean;
+	rowEl: (HTMLDivElement | null);
+}
+
+export type tableRows = IRows[];
+
+export interface ITableStructure{
+	isCached: boolean;
+	parentTableEl: (HTMLDivElement | null);
+	tableRows: tableRows;
+}
+
+export type handleStatsBtn = (this:HTMLButtonElement, e: MouseEvent)=>void;
+
+export type btnStateChanging = (btn:HTMLButtonElement)=>boolean;
+
+export type cacheTableStruct = ()=>boolean;
