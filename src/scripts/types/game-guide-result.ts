52628deb17be1b28d71b5baf142bdb53b84d559c
Michael Holod <git_ara@ukr.net>
2024-08-20 22:34:59 +0300

    Created types for the list of all levels. These levels are grouped by timestamp

--- src/scripts/types/game-guide-result.ts
+++ src/scripts/types/game-guide-result.ts
@@ -1,1 +1,17 @@
+import { IPreparedLevelData } from './game-core';
+
+export type levelsPack = Array<IPreparedLevelData>;
+
+export interface ILevelsPackData{
+	[id:number]: levelsPack;
+};
+
+export type stateLevelsData = Array<ILevelsPackData>;
+
+export type resultOnInitData = (panelResult:HTMLElement)=>boolean;
+
 export type changePanelsOnPage = (this:HTMLButtonElement, e:MouseEvent)=>void;
+
+export type subsOnData = (levelsAllData:IPreparedLevelData[])=>void;
+
+export type getDataFromStorage = ()=>stateLevelsData;
