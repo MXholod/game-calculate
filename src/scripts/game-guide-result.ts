a3e4ab1400d0de1e048f35bf0597e2e3046faadf
Michael Holod <git_ara@ukr.net>
2024-08-20 22:46:29 +0300

    Implemented 'getFromStorage' method, which receives data from localStorage and method 'resultOnInit' that displays the list of data on the result page

--- src/scripts/game-guide-result.ts
+++ src/scripts/game-guide-result.ts
@@ -1,5 +1,24 @@
-import { changePanelsOnPage } from './types/game-guide-result';
+import { resultOnInitData, stateLevelsData, changePanelsOnPage, subsOnData, getDataFromStorage } from './types/game-guide-result';
+import { IPreparedLevelData } from "./types/game-core";
 
+//The array of data of all levels
+export let stateLevels:stateLevelsData = [];
+
+//If data is in the localStorage the panel with data is showed otherwise the panel with no results is showed
+export const resultOnInit:resultOnInitData = (panelResult:HTMLElement):boolean=>{
+	if(!panelResult) return false;
+	const noResultBlock = (panelResult!.children[1] as HTMLDivElement);
+	const resultBlock = (panelResult!.children[2] as HTMLDivElement);
+	if(getFromStorage().length === 0){
+		noResultBlock.style.display = "block";
+		resultBlock.style.display = "none";
+		return false;
+	}else{
+		noResultBlock.style.display = "none";
+		resultBlock.style.display = "block";
+		return true;
+	}
+}
 //This function is responsible for switching between panels
 export const changePanels:changePanelsOnPage = function(this:HTMLButtonElement, e:MouseEvent):void{
 	if(this instanceof HTMLButtonElement){
@@ -19,4 +38,20 @@ export const changePanels:changePanelsOnPage = function(this:HTMLButtonElement, 
 			}
 		}
 	}
+}
+//This function subscribes on the levels data. It calls in user-Interaction file
+export const subscribesOnDataResult:subsOnData = (levelsAllData:IPreparedLevelData[]):void=>{
+	//The local array of - { level: 0, levelElapsedTime: 0, userResult: 0, cpuResult: 0, isSuccess: false } 
+	if(levelsAllData.length > 0){
+		console.log("All levels in results: ",levelsAllData);
+	}
+}
+//Get data from storage
+export const getFromStorage:getDataFromStorage = ():stateLevelsData=>{
+	if(window.localStorage.getItem('levels')){
+		const levelsJson:string = <string>(window.localStorage.getItem('levels'));
+		const levels = JSON.parse(levelsJson);
+		return levels;
+	}
+	return [];
 }
