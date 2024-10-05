4e190dd68939966030a8236a068a716094e5ec57
Michael Holod <git_ara@ukr.net>
2024-10-05 11:32:58 +0300

    Added types for a new class method that defines which function will be called after a pagination button is pressed

--- src/scripts/types/pagination-results.ts
+++ src/scripts/types/pagination-results.ts
@@ -1,5 +1,16 @@
-export interface IPagination{ 
+export enum Controls{
+	start = 'start',
+	end = 'end',
+	arrowLeft = 'arrow-left',
+	arrowRight = 'arrow-right',
+	cell1 = 'cell-1',
+	cell2 ='cell-2',
+	cell3 ='cell-3'
+}
+
+export interface IPagination{
 	applyPaginationBlockEvents(paginationBlock: HTMLDivElement):void;
+	detectPressedControl(controlName: Controls):boolean;
 }
 
 export type actPagination = (paginationHtmlBlock: HTMLDivElement)=>void;
